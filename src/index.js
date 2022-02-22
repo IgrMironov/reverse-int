f47a52869359c680e2b40d89a0aca82621908a70
IgrMironov <igrmironov@gmail.com>
2022-02-22 23:20:24 +0300

    fix: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let num = '';
+    n = n.toString();
+    if (n < 0) {
+        n = (-n).toString();
+    }
+        for (let i = n.length - 1; i >= 0; i--) {
+             num = num + n[i];
+        }
+    return num; 
 }
